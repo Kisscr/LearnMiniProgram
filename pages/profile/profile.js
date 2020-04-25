5a1f5adc44772bcdb19fce0f4902547537b9a4fc
kisscr <291331200@qq.com>
2020-04-25 22:01:04 +0800

    wx:if和wx:for以及block标签

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -5,9 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isShow: true,
+    score: 55,
+    movies: ['变形金刚', '盗梦空间', '大话西游'],
+    num: [
+      [10, 20, 30, 40],
+      [1, 2, 3, 4, 5],
+      [100, 200, 300, 500]
+    ]
   },
 
+  handleSwitchShow() {
+    this.setData({
+      isShow: !this.data.isShow
+    })
+  },
+
+  handleIncrement() {
+    this.setData({
+      score: this.data.score += 5
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
